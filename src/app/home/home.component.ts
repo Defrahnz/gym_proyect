f96cde36d493ccb657aa3b1833108427d2487552
Rickbro <ricoro845@gmail.com>
2025-08-15 21:28:42 -0600

    Agregar componentes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  imports: [RouterLink],
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {}
