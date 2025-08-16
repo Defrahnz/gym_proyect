b911be71a21a9b1596742ba125570a28a9213720
Rickbro <ricoro845@gmail.com>
2025-08-15 21:19:31 -0600

    Cambios en componente home y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SociosComponent } from './socios/socios.component';
+import { ContabilidadComponent } from './contabilidad/contabilidad.component';
+import { PagosComponent } from './pagos/pagos.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { PersonalComponent } from './personal/personal.component';
+import { InventarioComponent } from './inventario/inventario.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: LoginComponent },
+    { path: 'home', component: HomeComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'socios', component: SociosComponent},
+    { path: 'contabilidad', component: ContabilidadComponent},
+    { path: 'pagos', component: PagosComponent},
+    { path: 'estadisticas', component:  EstadisticasComponent},
+    { path: 'personal', component: PersonalComponent},
+    { path: 'inventario', component: InventarioComponent}
+
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [RouterLink],
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
